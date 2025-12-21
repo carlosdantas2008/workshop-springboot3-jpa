b723e2965f77c9745b31d02cdd6e6d41177aca62
Carlos Pereira <carlospereira@carloss-mbp.lan>
2025-12-20 18:28:03 -0800

    Order, Instant, ISO 8601 - part1

--- src/main/java/com/educandoweb/course/resource/UserResource.java
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,14 +1,12 @@
 package com.educandoweb.course.resource;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
